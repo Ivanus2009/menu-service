025746438158e62cd4d3e19af1545a89ce175d82
user <user@MacBook-Pro-user.local>
2025-06-18 19:11:42 +0300

    Force CORS: allow *

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const {
 
 const redis = new Redis(REDIS_URL);
 const app   = express();
-app.use(cors());
+app.use(cors({
+    origin: '*'   
+  }));
 app.use(express.json());
 
 // Функция, которая стягивает «сырые» данные
